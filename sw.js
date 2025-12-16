d6235fbe807b8f70d613d21944d5b90e2b9d5ba7
heradriel2001-cpu <heradriel2001@gmail.com>
2025-12-15 23:48:56 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = "finanzas-pwa-v2";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./AppImages/android-launchericon-48-48.png",
+  "./AppImages/android-launchericon-72-72.png",
+  "./AppImages/android-launchericon-96-96.png",
+  "./AppImages/android-launchericon-144-144.png",
+  "./AppImages/android-launchericon-192-192.png",
+  "./AppImages/android-launchericon-512-512.png"
+];
